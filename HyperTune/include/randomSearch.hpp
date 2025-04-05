3fb53a4d07c864f1fa55188e12ee10f12cf636cd
Niko Bellic <abhijitashanbhag@outlook.com>
2025-04-05 21:35:32 +0530

    Basic Header Implementations and some Class Implementations Check out the files themselves, too lazy to comment about it

--- HyperTune/include/randomSearch.hpp
+++ HyperTune/include/randomSearch.hpp
@@ -5,4 +5,21 @@
 //
 //  Created by Abhijit Shanbhag on 26/03/25.
 //
+#ifndef HYPERTUNE_RANDOM_SEARCH_HPP
+#define HYPERTUNE_RANDOM_SEARCH_HPP
+#include "searchStrategy.hpp"
+namespace hypertune{
+class RandomSearch : public SearchStrategy {
+public:
+    RandomSearch(const SearchSpace& searchSpace, std::mt19937& rng);
+    
+    /// Gnerate Random Configuration
+    Config nextConfiguration() override;
+    
+    /// Update with a new reuslt
+    void update(const EvaluationResult& result) override;
+};
+}
+
+#endif // HYPERTUNE_RANDOM_SEARCH_HPP
 
